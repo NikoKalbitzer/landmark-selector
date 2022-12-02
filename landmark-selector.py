0fb778a5f891d960666137b3d98885480c369c3f
kan42348 <niko.keineahnn>
2022-12-02 16:16:25 +0100

    added not visible LM support

--- landmark-selector.py
+++ landmark-selector.py
@@ -7,7 +7,7 @@ PATH_OUTPUT = "out/"
 FILENAME = "TestPictureLMsMean.png"
 PATH_PICTURE = f"images/{FILENAME}"
 
-landmarks = [["SN", 0, 0], ["N-L", 0, 0], ["N-R", 0, 0], ["XI", 0,0]]
+landmarks = [["SN", 0, 0, 1], ["N-L", 0, 0, 1], ["N-R", 0, 0, 1], ["XI", 0, 0, 1]]
 counter = 0
 
 
@@ -18,36 +18,38 @@ def get_name_without_ending(filename):
 def click_event(event, x, y, flags, params):
     global landmarks
     global counter
-    # checking for left mouse clicks
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
     if event == cv2.EVENT_LBUTTONDOWN:
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        # 70x30 schwarze box zeichnen
-        cv2.rectangle(img, (0, 0), (70, 30), (0, 0, 0), -1)
-        cv2.circle(img, (x, y), radius=4, color=(0, 0, 255), thickness=-1)
         if counter < len(landmarks):
+            cv2.circle(img, (x, y), radius=4, color=(0, 0, 255), thickness=-1)
             landmarks[counter][1] = x
             landmarks[counter][2] = y
             counter += 1
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        if counter < len(landmarks):
+            landmarks[counter][3] = 0
+            counter +=1
+
+    if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
+       # draw black box to override last letters
+        cv2.rectangle(img, (0, 0), (90, 30), (0, 0, 0), -1)
         if counter < len(landmarks):
             cv2.putText(img, landmarks[counter][0], (5, 25), font,
-                        1, (255, 0, 0), 2)
+                1, (255, 0, 0), 2)
         cv2.imshow('image', img)
         print(landmarks)
-
-    # checking for right mouse clicks
-    if event == cv2.EVENT_RBUTTONDOWN:
-        cv2.imshow('image', img)
 
 
 def write_to_file(lm_List):
     with open(PATH_OUTPUT + get_name_without_ending(FILENAME) + ".tlms", "w") as f:
         for line in lm_List:
-            f.write(f"{line[0]} 1 {line[1]} {line[2]}\n")  # so far always visible
+            f.write(f"{line[0]} {line[3]} {line[1]} {line[2]}\n")  # so far always visible
 
 
 # driver function
 if __name__ == "__main__":
-
     img = cv2.imread(PATH_PICTURE, 1)
     cv2.imshow('image', img)
     font = cv2.FONT_HERSHEY_SIMPLEX
